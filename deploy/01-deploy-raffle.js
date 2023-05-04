a5059f3c1a990f097db3ce76c0edcf4468143141
inu <j.inurria@gmail.com>
2023-05-04 13:35:48 +0200

    hardhat config

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,13 @@
+const { network } = require("hardhat")
+
+module.exports = async function ({ getNamedAccounts, deployment }) {
+  const { deploy, log } = deployment
+  const { deployer } = await getNamedAccounts()
+
+  const raffle = await deploy("Raffle", {
+    from: deployer, 
+    args: [],
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,66 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const MUMBAY_RPC_URL = process.env.MUMBAY_RPC_URL
+const BSC_RPC_URL = process.env.BSC_RPC_URL
+
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAM_API_KEY = process.env.ETHERSCAM_API_KEY || "API_KEY"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "API_KEY"
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6, // Esperar bloques en esta red
+    },
+    mumbay: {
+      url: MUMBAY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+      blockConfirmations: 6,
+    },
+    bsc: {
+      url: BSC_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 97,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      //acounts are give by hardhat
+      chainId: 31337,
+    },
+  },
+  //That is for validate (publish) the source code
+  etherscan: {
+    apiKey: ETHERSCAM_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
   solidity: "0.8.18",
+  nameAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 }
